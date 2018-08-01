ed5859f090b4454f29a1623e998e19a2c2009145
Christopher <conrad317@gmail.com>
2018-07-31 23:48:43 -0700

    scaffolding complete

--- src/components/auth-form/auth-form.js
+++ src/components/auth-form/auth-form.js
@@ -1,5 +1,5 @@
-import React from '../../../../../../Library/Caches/typescript/2.9/node_modules/@types/react';
-import PropTypes from '../../../../../../Library/Caches/typescript/2.9/node_modules/@types/prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const emptyState = {
   username: '',
@@ -69,3 +69,4 @@ AuthForm.propTypes = {
   onComplete: PropTypes.func,
   type: PropTypes.string,
 };
+//.
